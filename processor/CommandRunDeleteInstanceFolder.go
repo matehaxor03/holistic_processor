c83c3e507e03b686bfce266ff2e403c9ca0e1e6b
matehaxor03 <matehaxor03>
2022-12-25 11:46:53 +1100

    remove branch instances folder

--- processor/CommandRunDeleteInstanceFolder.go
+++ processor/CommandRunDeleteInstanceFolder.go
@@ -2,9 +2,10 @@ package processor
 
 import (
 	json "github.com/matehaxor03/holistic_json/json"
-	//common "github.com/matehaxor03/holistic_common/common"
-    //"path/filepath"
-	//"fmt"
+	common "github.com/matehaxor03/holistic_common/common"
+    "path/filepath"
+	"os"
+	"fmt"
 )
 
 func commandRunDeleteInstanceFolder(processor *Processor, request *json.Map, response_queue_result *json.Map) []error {
@@ -16,7 +17,24 @@ func commandRunDeleteInstanceFolder(processor *Processor, request *json.Map, res
 		errors = new_errors
 	}
 
-	// todo
+	// todo: validate directory names do things depending if branch or tag
+	directory_parts := common.GetDataDirectory()
+	directory_parts = append(directory_parts, "src")
+	directory_parts = append(directory_parts, *domain_name)
+	directory_parts = append(directory_parts, *repository_account_name)
+	directory_parts = append(directory_parts, *repository_name)
+	directory_parts = append(directory_parts, "branch_instances")
+	directory_parts = append(directory_parts, fmt.Sprintf("%d", *build_branch_instance_id))
+	full_path_of_directory := "/" + filepath.Join(directory_parts...)
+
+	if _, stat_error := os.Stat(full_path_of_directory); !os.IsNotExist(stat_error) {
+		bashCommand := common.NewBashCommand()
+		command := fmt.Sprintf("rm -fr %s", full_path_of_directory)
+		_, bash_command_errors := bashCommand.ExecuteUnsafeCommand(command)
+		if bash_command_errors != nil {
+			errors = append(errors, bash_command_errors...)
+		} 
+	}
 
 	if len(errors) > 0 {
 		return errors
