ad79f914c07ddb11dfb815b64229b2aba28b8bf6
matehaxor03 <matehaxor03>
2023-10-15 07:56:57 +1100

    wip create user

--- processor/CommandRunCreateUser.go
+++ processor/CommandRunCreateUser.go
@@ -2,6 +2,9 @@ package processor
 
 import (
 	json "github.com/matehaxor03/holistic_json/json"
+	common "github.com/matehaxor03/holistic_common/common"
+	"strconv"
+	"time"
 )
 
 func commandRunCreateUser(processor *Processor, request *json.Map, response_queue_result *json.Map) []error {
@@ -17,8 +20,37 @@ func commandRunCreateUser(processor *Processor, request *json.Map, response_queu
 		return errors
 	}
 
-	// todo
+	std_callback := getStdoutCallbackFunctionBranch(processor, *command_name, *branch_instance_id, *branch_instance_step_id, *branch_id, "")
+	stderr_callback := getStderrCallbackFunctionBranch(processor, *command_name, *branch_instance_id, *branch_instance_step_id, *branch_id, "")
 
+	time_format := "20060102"
+	// replace with created date of branch instance record
+	time_now := time.Now().UTC()
+	branch_instance_id_string := strconv.FormatUint(*branch_instance_id, 10)
+	if len(branch_instance_id_string) > 10 {
+		branch_instance_id_string = branch_instance_id_string[len(branch_instance_id_string)-10:]
+	}
+	
+	shell_command := "dscl . -create /Users/holistic_b" + time_now.Format(time_format) + branch_instance_id_string
+	bashCommand := common.NewBashCommand()
+
+	stdout_lines, bash_errors := bashCommand.ExecuteUnsafeCommandUsingFilesWithoutInputFile(shell_command)
+
+	if bash_errors != nil {
+		errors = append(errors, bash_errors...)
+	}
+
+	if stdout_lines != nil {
+		for _, stdout_line := range stdout_lines {
+			(*std_callback)(stdout_line)
+		}
+	}
+
+	if errors != nil {
+		for _, e := range errors {
+			(*stderr_callback)(e)
+		}
+	}
 
 	trigger_next_run_command_errors := triggerNextRunCommand(processor, command_name, branch_instance_step_id, branch_instance_id, branch_id, build_step_id, order, domain_name, repository_account_name, repository_name, branch_name, parameters, errors, request)
 	if trigger_next_run_command_errors != nil {
