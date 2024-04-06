1c0a8084fd09da2f7356b864399e8f74aceb682e
matehaxor03 <matehaxor03>
2024-04-06 11:33:52 +1100

    create tags folder

--- processor/CommandRunCreateTagsFolder.go
+++ processor/CommandRunCreateTagsFolder.go
@@ -2,9 +2,6 @@ package processor
 
 import (
 	json "github.com/matehaxor03/holistic_json/json"
-	common "github.com/matehaxor03/holistic_common/common"
-	"os"
-    "path/filepath"
 )
 
 func commandRunCreateTagsFolder(processor *Processor, request *json.Map, response_queue_result *json.Map) []error {
@@ -20,24 +17,38 @@ func commandRunCreateTagsFolder(processor *Processor, request *json.Map, respons
 		return errors
 	}
 
-	// todo: validate directory names
-	directory_parts := common.GetDataDirectory()
+	host_user := processor.GetHostUser()
+	home_directory, home_directory_errors := host_user.GetHomeDirectoryAbsoluteDirectory()
+	if home_directory_errors != nil {
+		errors = append(errors, home_directory_errors...)
+	}
+
+	if len(errors) > 0 {
+		trigger_next_run_command_errors := triggerNextRunCommand(processor, command_name, branch_instance_step_id, branch_instance_id, branch_id, build_step_id, order, domain_name, repository_account_name, repository_name, branch_name, parameters, created_date, errors, request)
+		if trigger_next_run_command_errors != nil {
+			errors = append(errors, trigger_next_run_command_errors...)
+		}
+		return errors
+	}
+
+	directory_parts := home_directory.GetPath()
 	directory_parts = append(directory_parts, "src")
 	directory_parts = append(directory_parts, *domain_name)
 	directory_parts = append(directory_parts, *repository_account_name)
 	directory_parts = append(directory_parts, *repository_name)
 	directory_parts = append(directory_parts, "tags")
-	full_path_of_directory := "/" + filepath.Join(directory_parts...)
+	
+	host_client := processor.GetHostClient()
+	branches_folder, branches_folder_errors := host_client.AbsoluteDirectory(directory_parts)
 
-	if _, stat_error := os.Stat(full_path_of_directory); os.IsNotExist(stat_error) {
-		permissions := int(0700)
-		create_directory_error := os.MkdirAll(full_path_of_directory, os.FileMode(permissions))
-		if create_directory_error != nil {
-			errors = append(errors, create_directory_error)
+	if branches_folder_errors != nil {
+		errors = append(errors, branches_folder_errors...)
+	} else {
+		create_if_does_not_exist_errors := branches_folder.CreateIfDoesNotExist()
+		if create_if_does_not_exist_errors != nil {
+			errors = append(errors, create_if_does_not_exist_errors...)
 		}
 	}
-
-	
 
 	trigger_next_run_command_errors := triggerNextRunCommand(processor, command_name, branch_instance_step_id, branch_instance_id, branch_id, build_step_id, order, domain_name, repository_account_name, repository_name, branch_name, parameters, created_date, errors, request)
 	if trigger_next_run_command_errors != nil {
