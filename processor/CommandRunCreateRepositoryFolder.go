4a71b314a944b945c4b9003b8397faedbbf9cac5
matehaxor03 <matehaxor03>
2024-04-06 11:28:30 +1100

    check create repostiroy folder

--- processor/CommandRunCreateRepositoryFolder.go
+++ processor/CommandRunCreateRepositoryFolder.go
@@ -2,8 +2,6 @@ package processor
 
 import (
 	json "github.com/matehaxor03/holistic_json/json"
-	"os"
-    "path/filepath"
 )
 
 func commandRunCreateRepositoryFolder(processor *Processor, request *json.Map, response_queue_result *json.Map) []error {
@@ -33,24 +31,23 @@ func commandRunCreateRepositoryFolder(processor *Processor, request *json.Map, r
 		return errors
 	}
 
-
-	// todo: validate directory names
 	directory_parts := home_directory.GetPath()
 	directory_parts = append(directory_parts, "src")
 	directory_parts = append(directory_parts, *domain_name)
 	directory_parts = append(directory_parts, *repository_account_name)
 	directory_parts = append(directory_parts, *repository_name)
-	full_path_of_directory := "/" + filepath.Join(directory_parts...)
+	
+	host_client := processor.GetHostClient()
+	repository_account_folder, repository_account_folder_errors := host_client.AbsoluteDirectory(directory_parts)
 
-	if _, stat_error := os.Stat(full_path_of_directory); os.IsNotExist(stat_error) {
-		permissions := int(0700)
-		create_directory_error := os.MkdirAll(full_path_of_directory, os.FileMode(permissions))
-		if create_directory_error != nil {
-			errors = append(errors, create_directory_error)
+	if repository_account_folder_errors != nil {
+		errors = append(errors, repository_account_folder_errors...)
+	} else {
+		create_if_does_not_exist_errors := repository_account_folder.CreateIfDoesNotExist()
+		if create_if_does_not_exist_errors != nil {
+			errors = append(errors, create_if_does_not_exist_errors...)
 		}
 	}
-
-	
 
 	trigger_next_run_command_errors := triggerNextRunCommand(processor, command_name, branch_instance_step_id, branch_instance_id, branch_id, build_step_id, order, domain_name, repository_account_name, repository_name, branch_name, parameters, created_date, errors, request)
 	if trigger_next_run_command_errors != nil {
